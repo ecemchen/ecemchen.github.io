0a829b6fc41d21acb199b43e5d815795822cde4d
Milena <135503893+mbsva@users.noreply.github.com>
2025-01-17 12:55:47 +0100

    removed imports

--- app/src/main/java/dev/christina/moonapp/ui/MoonListScreen.kt
+++ app/src/main/java/dev/christina/moonapp/ui/MoonListScreen.kt
@@ -6,8 +6,6 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -17,7 +15,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import dev.christina.moonapp.data.db.MoonEntity
 import java.time.LocalDate
 import java.time.Month
 import java.time.YearMonth
