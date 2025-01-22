04f479063e800b456bea8967286d2f0cb1354f0b
Chia1995 <cc231023@fhstp.ac.at>
2025-01-22 11:16:48 +0100

    deleted darkmode option in theme.kt

--- app/src/main/java/dev/christina/moonapp/ui/theme/Theme.kt
+++ app/src/main/java/dev/christina/moonapp/ui/theme/Theme.kt
@@ -11,12 +11,6 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
@@ -35,7 +29,6 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun MoonAppTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
@@ -43,10 +36,8 @@ fun MoonAppTheme(
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
             val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            dynamicLightColorScheme(context)
         }
-
-        darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
 
